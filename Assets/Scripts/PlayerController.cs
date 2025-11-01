b15d4f65795b594874c999ec19de4c392d0ee3f1
magopoligonal <magopoligonal@gmail.com>
2025-11-01 15:03:45 -0300

    01.01.00_SemDoubleJump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rigidBody;
     public float speed = 5f;
     public float jumpForce = 10f;
-    
+    private bool _isGrounded = false;
+    public LayerMask groundLayer;
+   
+
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -14,16 +17,26 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        movement();
+    }
+
+    private void movement()
+    {
         if (Input.GetAxis("Horizontal") != 0)
         {
-            rigidBody.linearVelocity = 
-                new Vector2(Input.GetAxis("Horizontal") * speed, 
+            rigidBody.linearVelocity =
+                new Vector2(Input.GetAxis("Horizontal") * speed,
                     rigidBody.linearVelocity.y);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
         {
             rigidBody.AddForceY(jumpForce);
+            _isGrounded = false;
         }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other) {
+        _isGrounded = true;
     }
 }
